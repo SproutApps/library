76c6c43854d4eed8193435e1057775f7d3c1c504
Dan Cameron <dan@sproutapps.co>
2015-09-30 10:11:17 -0700

    Filter stripe

--- functions/actions/invoices/change-default-status.php
+++
@@ -1,6 +0,0 @@
-<?php // don't include this in your functions.php
-
-function _si_change_default_status_to_pending( SI_Invoice $invoice ) {
-	$invoice->set_pending();
-}
-add_action( 'sa_new_invoice', '_si_change_default_status_to_pending' );
