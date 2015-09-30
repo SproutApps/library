647fd4e9df6106330ef46b7db8087411d9576a56
Dan Cameron <dan@sproutventure.com>
2015-09-30 10:15:59 -0700

    Update filter-stripe-js-attributes.php

--- snippets/add-ons/payments/filter-stripe-js-attributes.php
+++ snippets/add-ons/payments/filter-stripe-js-attributes.php
@@ -1,8 +1,12 @@
 <?php // don't include this in your functions.php
 
 function _remove_auto_email_adjust_logo( $attributes = array(), $invoice_id = 0 ) {
+	/**
+	 * Data attributes can be found in this documentation 
+	 * https://stripe.com/docs/checkout
+	 */
 	unset( $attributes['email'] );
-	$attributes['image'] = ( get_theme_mod( 'si_logo' ) ) ? esc_url( get_theme_mod( 'si_logo', si_doc_header_logo_url( $invoice_id ) ) ) : si_doc_header_logo_url( $invoice_id );
+	$attributes['image'] = '/img/documentation/checkout/marketplace.png';
 	return $attributes;
 }
 add_filter( 'si_stripe_js_data_attributes', '_remove_auto_email_adjust_logo' );
