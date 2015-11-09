3cc4db329464ed6fdcf9ea940f3290739e1b4718
Dan Cameron <dan@sproutapps.co>
2015-11-09 14:31:48 -0800

    fix?

--- snippets/invoices/change-id-on-recurring-invoices.php
+++ snippets/invoices/change-id-on-recurring-invoices.php
@@ -4,14 +4,16 @@ function maybe_change_invoice_id( $master_invoice_id = 0, $invoice_id = 0 ) {
 	if ( ! class_exists( 'SI_Invoices_Recurring' ) ) {
 		return;
 	}
+	// master invoice
+	$master_invoice = SI_Invoice::get_instance( $master_invoice_id );
+	$master_invoice_id = $master_invoice->get_invoice_id();
+
+	// build id
+	$clones = SI_Invoices_Recurring::get_child_clones( $master_invoice_id );
+	$new_id = $master_invoice_id . '-' . count( $clones );
+
+	// new invoice created
 	$invoice = SI_Invoice::get_instance( $invoice_id );
-
-	$clones = SI_Invoices_Recurring::get_child_clones( $invoice_id );
-	$clone_count = count( $clones );
-
-	$invoice_id = $invoice->get_invoice_id();
-	$new_id = $invoice_id . '-' . $clone_count;
-
 	$invoice->set_invoice_id( $new_id );
 }
 add_action( 'si_recurring_invoice_created', 'maybe_change_invoice_id', 10, 2 );
